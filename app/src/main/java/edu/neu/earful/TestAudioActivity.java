4841fb23ca227afc351e80ca4b3db5ee9a964427
jjviasus <61569318+jjviasus@users.noreply.github.com>
2022-04-08 11:09:32 -0400

    buttons moved off of main activity

---
+++ app/src/main/java/edu/neu/earful/TestAudioActivity.java
@@ -0,0 +1,59 @@
+package edu.neu.earful;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
+
+public class TestAudioActivity extends AppCompatActivity {
+    Button playAudioButton;
+    Button stopAudioButton;
+    MediaPlayer player;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_audio);
+
+        playAudioButton = findViewById(R.id.play_button);
+        stopAudioButton = findViewById(R.id.stop_button);
+
+        playAudioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (player == null) {
+                    player = new MediaPlayer();
+                    AssetFileDescriptor afd = null;
+                    try {
+                        afd = getAssets().openFd("Boost_4kHz_+12db.mp3");
+                        player.setDataSource(afd.getFileDescriptor(),afd.getStartOffset(),afd.getLength());
+                        player.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                            @Override
+                            public void onPrepared(MediaPlayer mediaPlayer) {
+                                player.start();
+                            }
+                        });
+                        player.prepareAsync();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        stopAudioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (player!= null) {
+                    player.release();
+                    player = null;
+                }
+            }
+        });
+    }
+}
