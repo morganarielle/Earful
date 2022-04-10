b5fff1c452de494486f6d039be15cfb2236864e1
Alex Reed <alexreed100@gmail.com>
2022-04-10 16:10:41 -0400

    create basic screen flow

--- app/src/main/java/edu/neu/earful/MainActivity.java
+++ app/src/main/java/edu/neu/earful/MainActivity.java
@@ -1,7 +1,10 @@
 package edu.neu.earful;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onLogIn(View view){
+        Intent activity2Intent = new Intent(getApplicationContext(), MainMenuActivity.class);
+        startActivity(activity2Intent);
     }
 }
---
+++ app/src/main/java/edu/neu/earful/MainMenuActivity.java
@@ -0,0 +1,26 @@
+package edu.neu.earful;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+    public void launchMusicianMode(View view){
+        Intent activity2Intent = new Intent(getApplicationContext(), MusicianActivity.class);
+        startActivity(activity2Intent);
+    }
+
+    public void launchMixingMode(View view){
+        Intent activity2Intent = new Intent(getApplicationContext(), MixingActivity.class);
+        startActivity(activity2Intent);
+    }
+}
