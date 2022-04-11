d3f2d258aa429ed9c83c3c6f0cbb287078de5db5
Alex Reed <alexreed100@gmail.com>
2022-04-11 18:39:59 -0400

    add username input to login screen

--- app/src/main/java/edu/neu/earful/MainActivity.java
+++ app/src/main/java/edu/neu/earful/MainActivity.java
@@ -4,18 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private Button signInButton;
+    private EditText usernameText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_mai
+        signInButton = findViewById(R.id.signin);
+        usernameText = findViewById(R.id.username);
+        usernameText.addTextChangedListener(editTextWatcher);
+
     }
 
-    public void onLogIn(View view){
+    public void onLogIn(View view) {
         Intent activity2Intent = new Intent(getApplicationContext(), MainMenuActivity.class);
         startActivity(activity2Intent);
     }
+
+    TextWatcher editTextWatcher = new TextWatcher() {
+
+
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            // Enable the button if a username is entered
+            signInButton.setEnabled(!signInButton.getText().toString().equals(""));
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+        }
+    };
 }
