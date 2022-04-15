24783afdce006217f8b5feed0bf801d3062062b0
jjviasus <61569318+jjviasus@users.noreply.github.com>
2022-04-15 19:14:22 -0400

    check boxes added

--- app/src/main/java/edu/neu/earful/MenuActivity.java
+++ app/src/main/java/edu/neu/earful/MenuActivity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainMenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
